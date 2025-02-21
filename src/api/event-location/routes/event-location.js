162bc70ccc63f26501000647669a9a1e226e0464
alanllamas <alanllamasg@gmail.com>
2025-02-21 12:31:46 -0600

    feat: added location as component

--- src/api/event-location/routes/event-location.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * event-location router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::event-location.event-location');
