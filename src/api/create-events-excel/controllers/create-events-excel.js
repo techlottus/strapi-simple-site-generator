12842cbdd953f8f6868c7f0282376187cfc1feda
alanllamas <alanllamasg@gmail.com>
2025-04-15 13:20:09 -0500

    feat: added excel file model

---
+++ src/api/create-events-excel/controllers/create-events-excel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * create-events-excel controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::create-events-excel.create-events-excel');
---
+++ src/api/create-events-excel/routes/create-events-excel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * create-events-excel router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::create-events-excel.create-events-excel');
