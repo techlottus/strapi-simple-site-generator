6f16759b4edcc2cf670f33e53f5906c360603e50
alan llamas <alanllamasg@gmail.com>
2025-04-30 14:36:41 -0600

    chore: added salt token to admin config

--- config/admin.js
+++ config/admin.js
@@ -2,6 +2,11 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
